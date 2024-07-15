50973e5141809378c0530eca4bc0b34dc9830ba1
Vincent Tran <vintran2015@gmail.com>
2024-07-14 20:43:34 -0400

    Switching from standalone daemon to systemd service

--- nft-client-manager.cpp
+++ nft-client-manager.cpp
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#include <csignal>
 #include <unistd.h>
 #include <stdlib.h>
 
@@ -10,48 +11,19 @@
 
 using namespace std;
 
-static string nftconf = "/etc/ncm/ncm.conf";
+volatile sig_atomic_t sigterm = 0;
+
+void handleSIGTERM(int signum) {
+    cout << "Recieved SIGTERM, shutting down..." << endl;
+    sigterm = 1;
+}
 
 int main(void) {
-    daemonize();
-    return 0;
+    signal(SIGTERM, handleSIGTERM);
+
+    while (!sigterm) {
+        // Main loop happens here
+    }
+
+    // Gracefully shutdown service
 } 
-
-/**
- * Daemonizes the current running script
-*/
-void daemonize() {
-    pid_t pid = fork(); // fork the current proccess and return it's process id
-
-    // if pid > 0, we know this is currently the parent process (and that the child process ran successfully) so we close it
-    if (pid > 0) {
-        exit(EXIT_SUCCESS);
-    }
-    // negative pid means an error... so exit if an error occured
-    else if (pid < 0) {
-        exit(EXIT_FAILURE);
-    }
-
-    // load old nftables config
-    string command = "nft -f " + nftconf;
-    system(command.c_str());
-
-    // main daemon loop
-    while (1) {
-        // todo:
-        //  - listen for incoming ncm join requests
-        //  - update nftables conf to allow clients to join ncm and talk to this computer
-        //      - authenticate with a shared secret
-        writeToConfig();
-        sleep(3);   // wait 3 seconds before looping again
-    }
-
-}
-
-/**
- * Writes current ncm table configuration to the configuration file
-*/
-void writeToConfig() {
-    string command = "nft list table inet ncm-table > " + nftconf;
-    system(command.c_str());
-}
